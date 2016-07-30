3bccb7eaff468d13ed1e71cde8bdedb606e86b5b
athom <athom@126.com>
2016-07-30 17:32:32 +0800

    fix docker script

--- cmd/yeerchesswebserver/docker_dev.sh
+++ cmd/yeerchesswebserver/docker_dev.sh
@@ -13,10 +13,9 @@ sudo docker rm -f $app_name
 
 sudo docker run -d --name $app_name \
             -p 3000:3000 \
-            #-v /Users/yeer/go/src:/go/src\
+            -v /Users/yeer/go/src:/go/src\
             athom/chess \
             /bin/bash \
-            -c "/go/bin/$app_name"
-            #-c "cd /go/src/github.com/athom/chess/cmd/$app_name; go build; go install; /go/bin/$app_name"
+            -c "cd /go/src/github.com/athom/chess/cmd/$app_name; go build; go install; /go/bin/$app_name"
 
 sudo docker logs -f $app_name
